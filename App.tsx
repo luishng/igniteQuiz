8ca9c87cd4938e71254b0cdb740d036f32b4f6fc
Luis Henrique <luishng@gmail.com>
2024-09-26 12:09:35 -0300

    Add: Animation on Questions and Flatlist

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
@@ -14,13 +17,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <GestureHandlerRootView style={{flex: 1}}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </GestureHandlerRootView>
   );
 }
